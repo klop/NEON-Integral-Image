e7a31924daa295fb41aa8986a4016ab951047a40
dara <dara.farimani@gmail.com>
2016-03-24 01:50:37 -0700

    logic for additional top row/left column with 0s in place

--- NEONIntegralImage.c
+++ NEONIntegralImage.c
@@ -13,12 +13,15 @@ void neon_integral_image(const uint8_t *sourceImage, uint32_t *integralImage,
                          size_t width, size_t height)
 {
     // a vector used later for shifting bytes and replacing with 0 using vector extraction
-    uint16x8_t extractionPlaceHolder = vdupq_n_u16(0);
+    uint16x8_t zeroVec = vdupq_n_u16(0);
 
-    // used to store an offset into a row
-    size_t j = 0;
+    // integral images add an extra row and column of 0s to the image
+    size_t integralImageWidth = width + 1;
 
-    // used to carry over the last prefix sum of a row segment to the next segment
+    // pointer to the start of the integral image, skipping past the first row and column
+    uint32_t *integralImageStart = integralImage + integralImageWidth + 1;
+
+    //  used to carry over the last prefix sum of a row segment to the next segment
     uint32_t prefixSumLastElement = 0;
 
     // prefix sum for rows
@@ -27,17 +30,20 @@ void neon_integral_image(const uint8_t *sourceImage, uint32_t *integralImage,
         // vector to carry over last prefix sum value to next chunk
         uint32x4_t carry = vdupq_n_u32(0);
 
-        // get offset to start of row in image buffer
-        size_t startAddress = i * width;
+        // get offset to start of row, taking into account that we have one more column and row than the source image
+        size_t sourceRowOffset = i * width;
 
-        // reset j offset to beginning of row buffer
-        j = 0;
+        // from the integral image start, this gives us an offset to the beginning of the next row, skipping the 0 column
+        size_t integralRowOffset = i * integralImageWidth;
 
-        // for every 16 bytes, iterate over the row
+        // count how many bytes we've passed over
+        size_t j = 0;
+
+        // iterate over the row in 16 byte chunks
         for (; j + 16 < width; j += 16) {
 
-            // load 16 pixels from given offset
-            uint8x16_t elements = vld1q_u8(sourceImage + startAddress + j);
+            // load 16 bytes/ints from given offset
+            uint8x16_t elements = vld1q_u8(sourceImage + sourceRowOffset + j);
 
             // take lower 8 8-bit ints
             uint8x8_t lowElements8 = vget_low_u8(elements);
@@ -52,30 +58,30 @@ void neon_integral_image(const uint8_t *sourceImage, uint32_t *integralImage,
             uint16x8_t highElements16 = vmovl_u8(highElements8);
 
             // add lowElements16 to lowElements16 shifted 2 bytes (1 element) to the right
-            lowElements16 = vaddq_u16(lowElements16, vextq_u16(extractionPlaceHolder, lowElements16, 7));
+            lowElements16 = vaddq_u16(lowElements16, vextq_u16(zeroVec, lowElements16, 7));
 
             // add result to result shifted 4 bytes (2 elements) to the right
-            lowElements16 = vaddq_u16(lowElements16, vextq_u16(extractionPlaceHolder, lowElements16, 6));
+            lowElements16 = vaddq_u16(lowElements16, vextq_u16(zeroVec, lowElements16, 6));
 
             // add result to result shifted 8 bytes (4 elements) to the right, we now have the prefix sums for this section
-            lowElements16 = vaddq_u16(lowElements16, vextq_u16(extractionPlaceHolder, lowElements16, 4));
+            lowElements16 = vaddq_u16(lowElements16, vextq_u16(zeroVec, lowElements16, 4));
 
             // do the same 3 steps above for highElements16
-            highElements16 = vaddq_u16(highElements16, vextq_u16(extractionPlaceHolder, highElements16, 7));
-            highElements16 = vaddq_u16(highElements16, vextq_u16(extractionPlaceHolder, highElements16, 6));
-            highElements16 = vaddq_u16(highElements16, vextq_u16(extractionPlaceHolder, highElements16, 4));
+            highElements16 = vaddq_u16(highElements16, vextq_u16(zeroVec, highElements16, 7));
+            highElements16 = vaddq_u16(highElements16, vextq_u16(zeroVec, highElements16, 6));
+            highElements16 = vaddq_u16(highElements16, vextq_u16(zeroVec, highElements16, 4));
 
             // take lower 4 16-bit ints of lowElements16, convert to 32 bit, and add to carry (carry is 0s for the first 8 pixels in each row)
             uint32x4_t lowElementsOfLowPrefix32 = vaddq_u32(vmovl_u16(vget_low_u16(lowElements16)), carry);
 
             // store lower 4 32-bit ints at appropriate offset: (X|O|O|O)
-            vst1q_u32(integralImage + startAddress + j + 0, lowElementsOfLowPrefix32);
+            vst1q_u32(integralImageStart + integralRowOffset + j, lowElementsOfLowPrefix32);
 
             // take upper 4 16-bit ints of lowElements16, convert to 32 bit, and add to carry
             uint32x4_t highElementsOfLowPrefix32 = vaddq_u32(vmovl_u16(vget_high_u16(lowElements16)), carry);
 
             // store upper 4 32-bit ints at appropriate offset: (O|X|O|O)
-            vst1q_u32(integralImage + startAddress + j + 4, highElementsOfLowPrefix32);
+            vst1q_u32(integralImageStart + integralRowOffset + j + 4, highElementsOfLowPrefix32);
 
             // take the last prefix sum from the second 32-bit vector to be added to the next prefix sums
             prefixSumLastElement = vgetq_lane_u32(highElementsOfLowPrefix32, 3);
@@ -87,13 +93,13 @@ void neon_integral_image(const uint8_t *sourceImage, uint32_t *integralImage,
             uint32x4_t lowElementsOfHighPrefix32 = vaddq_u32(vmovl_u16(vget_low_u16(highElements16)), carry);
 
             // store lower 4 32-bit ints at appropriate offset: (O|O|X|O)
-            vst1q_u32(integralImage + startAddress + j + 8, lowElementsOfHighPrefix32);
+            vst1q_u32(integralImageStart + integralRowOffset + j + 8, lowElementsOfHighPrefix32);
 
             // take upper 4 16-bit ints of lowElements16, convert to 32 bit, and add to carry
             uint32x4_t highElementsOfHighPrefix32 = vaddq_u32(vmovl_u16(vget_high_u16(highElements16)), carry);
 
             // store upper 4 32-bit ints at appropriate offset (O|O|O|X)
-            vst1q_u32(integralImage + startAddress + j + 12, highElementsOfHighPrefix32);
+            vst1q_u32(integralImageStart + integralRowOffset + j + 12, highElementsOfHighPrefix32);
 
             // take the last prefix sum from the second 32-bit vector to be added to the next prefix sums
             prefixSumLastElement = vgetq_lane_u32(highElementsOfHighPrefix32, 3);
@@ -106,45 +112,52 @@ void neon_integral_image(const uint8_t *sourceImage, uint32_t *integralImage,
         for (; j < width ; ++j) {
 
             // take the last prefix sum value and add the 8-bit int value from the source image at the appropriate index
-            prefixSumLastElement += sourceImage[startAddress + j];
+            prefixSumLastElement += sourceImage[sourceRowOffset + j];
 
             // set the value of the integral image to the last prefix sum, at the same index
-            integralImage[startAddress + j] = prefixSumLastElement;
+            integralImageStart[integralRowOffset + j] = prefixSumLastElement;
         }
     }
 
-    //prefix sum for columns (mb change this to height - 1?)
-    for (size_t i = 1; i < height; ++i) {
+    // prefix sum for columns, using height - 1 since we're taking pairs of rows
+    for (size_t i = 0; i < height - 1; ++i) {
         size_t j = 0;
+        size_t integralRowOffset = i * integralImageWidth;
 
         for (; j + 16 < width; j += 16) {
 
-            // load 4 32-bit ints from row 1
-            uint32x4_t row1Elements32 = vld1q_u32((integralImage + ((i - 1) * width) + j + 0));
+            // load 4 32-bit ints from row i (first row)
+            uint32x4_t row1Elements32 = vld1q_u32(integralImageStart + integralRowOffset + j);
 
-            // load 4 32-bit ints from row 2
-            uint32x4_t row2Elements32 = vld1q_u32((integralImage + (i  * width) + j + 0));
+            // load 4 32-bit ints from row i + 1 (second row)
+            uint32x4_t row2Elements32 = vld1q_u32(integralImageStart + integralRowOffset + integralImageWidth + j);
 
-            // add row 1 to row 2, giving the prefix sum of the second row
+            // add first row to second row, giving the prefix sum for the second row
             row2Elements32 = vqaddq_u32(row1Elements32, row2Elements32);
+
+            // replace the stored second row values with the prefix sum values
+            vst1q_u32(integralImageStart + integralRowOffset + integralImageWidth + j, row2Elements32);
 
             // do the same for the next 3 offsets (4, 8, 12), completing a 128-bit chunk
-            row1Elements32 = vld1q_u32((integralImage + ((i - 1) * width) + j + 4));
-            row2Elements32 = vld1q_u32((integralImage + (i  * width) + j + 4));
+            row1Elements32 = vld1q_u32(integralImageStart + integralRowOffset + j + 4);
+            row2Elements32 = vld1q_u32(integralImageStart + integralRowOffset + integralImageWidth + j + 4);
             row2Elements32 = vqaddq_u32(row1Elements32, row2Elements32);
+            vst1q_u32(integralImageStart + integralRowOffset + integralImageWidth + j + 4, row2Elements32);
 
-            row1Elements32 = vld1q_u32((integralImage + ((i - 1) * width) + j + 8));
-            row2Elements32 = vld1q_u32((integralImage + (i  * width) + j + 8));
+            row1Elements32 = vld1q_u32(integralImageStart + integralRowOffset + j + 8);
+            row2Elements32 = vld1q_u32(integralImageStart + integralRowOffset + integralImageWidth + j + 8);
             row2Elements32 = vqaddq_u32(row1Elements32, row2Elements32);
+            vst1q_u32(integralImageStart + integralRowOffset + integralImageWidth + j + 8, row2Elements32);
 
-            row1Elements32 = vld1q_u32((integralImage + ((i - 1) * width) + j + 12));
-            row2Elements32 = vld1q_u32((integralImage + (i  * width) + j + 12));
+            row1Elements32 = vld1q_u32(integralImageStart + integralRowOffset + j + 12);
+            row2Elements32 = vld1q_u32(integralImageStart + integralRowOffset + integralImageWidth + j + 12);
             row2Elements32 = vqaddq_u32(row1Elements32, row2Elements32);
+            vst1q_u32(integralImageStart + integralRowOffset + integralImageWidth + j + 12, row2Elements32);
         }
         
         // now handle the remainders
         for (; j < width; ++j) {
-            integralImage[ i * width + j] += integralImage[ (i - 1) * width + j];
+            integralImageStart[integralRowOffset + integralImageWidth + j] += integralImageStart[integralRowOffset + j];
         }
     }
 }
